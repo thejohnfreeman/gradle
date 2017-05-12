f62807a0c2a0842a3d1adeb6f22609463633e3a3
Paul Merlin <paul@gradle.com>
2017-05-12 13:38:53 +0200

    Increase distribution jar count now that gradle-script-kotlin-tooling-models is published

--- subprojects/distributions/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
+++ subprojects/distributions/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
@@ -38,7 +38,7 @@ abstract class DistributionIntegrationSpec extends AbstractIntegrationSpec {
     abstract String getDistributionLabel()
 
     int getLibJarsCount() {
-        160
+        161
     }
 
     def "no duplicate entries"() {
